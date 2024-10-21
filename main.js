c71538cc6ff181aa85d03af4c00f7427cc8a1367
jnt677 <jnt677677@gmail.com>
2024-10-22 00:54:44 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function kirimPesan() {
+
+    var nama = document.getElementById('nama');
+    var email = document.getElementById('email');
+    var pesan = document.getElementById('pesan');
+
+    var gabungan = 'Nama%3A%0A' + nama.value + '%0AEmail%3A%0A' + email.value + '%0APesan%3A%0A' + pesan.value;
+
+    var token = '7735969787:AAF_HqQQBI88oTL7PEWMKHGPU8XA6t3zY2s'; // Ganti dengan token bot yang kamu buat
+    var grup = '6731968814'; // Ganti dengan chat id dari bot yang kamu buat
+
+    $.ajax({
+        url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+        method: `POST`,
+    })
+}
