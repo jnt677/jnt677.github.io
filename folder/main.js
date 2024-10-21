ded536f9dd0051423f9d722c96b3617ae0db634c
jnt677 <jnt677677@gmail.com>
2024-10-22 02:46:21 +0700

    Update main.js

--- folder/main.js
+++ folder/main.js
@@ -2,9 +2,10 @@ function kirimPesan() {
 
     var nama = document.getElementById('nama');
     var email = document.getElementById('email');
+    var options = document.getElementById('options');
     var pesan = document.getElementById('pesan');
 
-    var gabungan = 'Nama%3A%0A' + nama.value + '%0AEmail%3A%0A' + email.value + '%0APesan%3A%0A' + pesan.value;
+    var gabungan = 'Nama%3A%0A' + nama.value + '%0AEmail%3A%0A' + email.value + '%0AOptions%3A%0A' + options.value;+ '%0APesan%3A%0A' + pesan.value;
 
     var token = '7735969787:AAF_HqQQBI88oTL7PEWMKHGPU8XA6t3zY2s'; // Ganti dengan token bot yang kamu buat
     var grup = '6731968814'; // Ganti dengan chat id dari bot yang kamu buat
